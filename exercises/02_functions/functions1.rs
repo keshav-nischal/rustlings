4810678ca8e371c4d11ebd80e74c97de9d1bf2bf
Keshav Nischal <91429557+keshav-nischal@users.noreply.github.com>
2024-01-29 09:15:46 +0000

    day1

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -3,8 +3,10 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+fn call_me(){
+    let x: u32 = 3000;
+    println!("hellow worlds, love you {}", x);
+}
 fn main() {
     call_me();
 }
